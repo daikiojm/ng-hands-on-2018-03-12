34df1c00ec30ec9297173054febd35bc67f4d309
daikiojm <daiking.ca2@gmail.com>
2018-03-10 18:00:40 +0900

    update(Toolbar): update style of toolbar component

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  title = 'gh-client';
 
   constructor() { }
 
