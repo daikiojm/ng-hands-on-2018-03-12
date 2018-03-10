caa72eb8dce5a2aedcd6704b1dcab0fa412dd2f5
daikiojm <daiking.ca2@gmail.com>
2018-03-10 17:56:58 +0900

    update(Toolbar): added toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
